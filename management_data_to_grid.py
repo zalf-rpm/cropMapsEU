783649322826ac620974ef7259c639d2756fa2ac
sanny <38938782+sannyschulz@users.noreply.github.com>
2021-05-26 21:24:15 +0200

    date correction

--- management_data_to_grid.py
+++ management_data_to_grid.py
@@ -12,29 +12,76 @@ def build() :
     gridFile = "./stu_eu_layer_grid.csv"
     pathSowingStartM = "./asciigrids_debug/Maize.crop.calendar.fill/plant.start.asc"
     pathSowingEndM = "./asciigrids_debug/Maize.crop.calendar.fill/plant.end.asc"
-    pathEndHarvestM = "./asciigrids_debug/Maize.crop.calendar.fill/harvest.end.asc"
+    pathEndHarvestM = "./asciigrids_debug/Maize.crop.calendar.fill/harvest.asc"
 
     pathSowingStartWW = "./asciigrids_debug/Wheat.Winter.crop.calendar.fill/plant.start.asc"
     pathSowingEndWW = "./asciigrids_debug/Wheat.Winter.crop.calendar.fill/plant.end.asc"
-    pathEndHarvestWW = "./asciigrids_debug/Wheat.Winter.crop.calendar.fill/harvest.end.asc"
+    pathEndHarvestWW = "./asciigrids_debug/Wheat.Winter.crop.calendar.fill/harvest.asc"
 
-    outFiles = [
-            "./stu_eu_layer_grid_management_Wheat_Winter.csv",
-            "./stu_eu_layer_grid_management_Maize.csv",
-    ]
+    outFile = "./stu_eu_layer_grid_management.csv"
     
+    lookup = readGridLookup(gridFile)
 
     # create a lat/lon map, assigning each Datapoint a lat/lon value
     # interpolate for each stu_eu grid point the matching data point
-    for idx in range(len(pathToGrids)):
-        file = pathToGrids[idx]
-        print("File:", file)
+
+    def loadDates(file, dateKey) :
         header = readAsciiHeader(file)
         ascii_data_array = np.loadtxt(header.ascii_path, dtype=np.float, skiprows=6)
-        # Set the nodata values to nan
-        ascii_data_array[ascii_data_array == header.ascii_nodata] = np.nan
-        GenerateGridLookup(gridFile, outFiles[idx], ascii_data_array, header.ascci_cols, header.ascii_rows)
+        if ascii_data_array.ndim == 1 :
+            reshaped_to_2d = np.reshape(ascii_data_array, (header.ascii_rows,-1 ))
+            ascii_data_array = reshaped_to_2d
+        ascii_data_array[ascii_data_array == header.ascii_nodata] = -1
 
+        colsLon = header.ascci_cols
+        rowsLat = header.ascii_rows
+        for key in lookup :
+            date = lookup[key]
+            # calculate position of lon/lat in array
+            arrPosCol = ((colsLon//2) + int(date.lon*12.))
+            arrPosRow = ((rowsLat//2) + int(-1*date.lat*12.))
+            value = int(ascii_data_array[arrPosRow][arrPosCol])
+            if value > 0 and value < 365 :
+                lookup[key].dates[dateKey] = value
+
+    loadDates( pathSowingStartM,"sowStartM")
+    loadDates(pathSowingStartWW ,"sowStartW")
+    loadDates( pathSowingEndM,"sowEndM")
+    loadDates( pathSowingEndWW,"sowEndW")
+    loadDates( pathEndHarvestM,"harvestM")
+    loadDates(pathEndHarvestWW ,"harvestW")
+
+    counter1 = 0
+    counter2 = 0
+    for key in lookup :
+        if lookup[key].dates["harvestM"] > lookup[key].dates["sowStartW"] :
+            counter1 += 1
+            lookup[key].dates["harvestM"] = 257
+            if lookup[key].dates["sowStartW"] < 258 :
+                lookup[key].dates["sowStartW"] = 258
+ 
+            if lookup[key].dates["sowEndW"] < lookup[key].dates["sowStartW"] :
+                counter2 += 1
+                lookup[key].dates["sowEndW"] = lookup[key].dates["sowStartW"]+14
+
+    print(counter1, counter2)
+
+    outGridHeader = "soil_ref,latitude,longitude,sowStartM,sowStartW,sowEndM,sowEndW,harvestM,harvestW \n"
+    with open(outFile, mode="wt", newline="") as outGridfile :
+        outGridfile.writelines(outGridHeader)
+        for key in lookup : 
+            data = lookup[key]
+            outline = [str(data.soilref),
+                        str(data.lat), #lat
+                        str(data.lon), #long
+                        str(data.dates["sowStartM"]),
+                        str(data.dates["sowStartW"]),
+                        str(data.dates["sowEndM"]),
+                        str(data.dates["sowEndW"]),
+                        str(data.dates["harvestM"]),
+                        str(data.dates["harvestW"])
+                        ]
+            outGridfile.writelines(",".join(outline) + "\n")
 @dataclass
 class AsciiHeader:
     ascii_path: str
@@ -71,50 +118,48 @@ def readAsciiHeader(ascii_path) :
 
     return AsciiHeader(ascii_path, ascci_cols, ascii_rows, ascii_xll, ascii_yll, ascii_cs, ascii_nodata, image_extent)
 
-def GenerateGridLookup(gridsource, outFile, npArr, colsLon, rowsLat) :
-    outGridHeader = "Column,Row,latitude,longitude,irrigation\n"
-    with open(outFile, mode="wt", newline="") as outGridfile :
-        outGridfile.writelines(outGridHeader)
-        with open(gridsource) as sourcefile:
-            firstLine = True
-            colID = -1
-            rowID = -1
-            latID = -1
-            lonID = -1
-            for line in sourcefile:
-                tokens = line.strip().split(",")          
-                if firstLine :
-                    # read header
-                    firstLine = False
-                    for index in range(len(tokens)) :
-                        token = tokens[index]
-                        if token == "Column_":
-                            colID = index
-                        if token == "Row":
-                            rowID= index
-                        if token == "latitude" :
-                            latID = index
-                        if token == "longitude" :
-                            lonID = index
-                else :
-                    col = int(tokens[colID])
-                    row = int(tokens[rowID])
-                    lat = float(tokens[latID])
-                    lon = float(tokens[lonID])
-                    # calculate position of lon/lat in array
-                    arrPosCol = ((colsLon//2) + int(lon*12.))
-                    arrPosRow = ((rowsLat//2) + int(-1*lat*12.))
-                    value = npArr[arrPosRow][arrPosCol]
-                    isIrrigated = 0
-                    if value > 0 :
-                        isIrrigated = 1
-                    outline = [str(col), #col 
-                                str(row), #row
-                                str(lat), #lat
-                                str(lon), #long
-                                str(isIrrigated) #irrigated
-                                ]
-                    outGridfile.writelines(",".join(outline) + "\n")
+@dataclass
+class DateEntry:
+    soilref: int
+    lat: float  
+    lon: float
+    dates: dict
+
+
+def readGridLookup(gridsource) :
+    lookup = dict()    
+    with open(gridsource) as sourcefile:
+        firstLine = True
+        soilrefID = -1
+        latID = -1
+        lonID = -1
+        for line in sourcefile:
+            tokens = line.strip().split(",")          
+            if firstLine :
+                # read header
+                firstLine = False
+                for index in range(len(tokens)) :
+                    token = tokens[index]
+                    if token == "latitude" :
+                        latID = index
+                    if token == "longitude" :
+                        lonID = index
+                    if token == "soil_ref" :
+                        soilrefID = index
+            else :
+                soilref = int(tokens[soilrefID])
+                lat = float(tokens[latID])
+                lon = float(tokens[lonID])
+                if not (soilref in lookup) :
+                    datesDic = dict()
+                    datesDic["sowStartM"] = 74
+                    datesDic["sowStartW"] = 288
+                    datesDic["sowEndM"] = 135
+                    datesDic["sowEndW"] = 319
+                    datesDic["harvestM"] = 287
+                    datesDic["harvestW"] = 248
+                    lookup[soilref] = DateEntry(soilref, lat, lon, datesDic)
+    return lookup    
 
 if __name__ == "__main__":
     build()
